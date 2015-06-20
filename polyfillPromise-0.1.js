ed58b498d897cf21e780df198b8d499ce908a729
Stephen Giles <steve@stephengil.es>
2015-06-20 20:30:51 +0100

    then() and catch() now return new Promise per ES6 spec. and minified version added

--- polyfillPromise-0.1.js
+++ polyfillPromise-0.1.js
@@ -1,29 +1,35 @@
 // version 0.1
 // polyfill ES6 Promise in older browsers
 // Stephen Giles. https://github.com/ingenious
+// API: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Promise
 (function() {
-    var PolyfillPromise, root = this;
-    if (!root.Promise) {
+    var newAble, PolyfillPromise, root = this;
+    if (root.Promise && typeof root.Promise == 'function') {
+        newAble = new root.Promise(function() {});
+    }
+    // check for compatible API
+    if (!newAble ||
+        !root.Promise.all || !root.Promise.race || !root.Promise.reject || !root.Promise.resolve ||
+        !newAble.then || !newAble['catch']
+    ) {
         PolyfillPromise = function Promise(fn) {
             var pp = this,
-                onResolve=[], rejectedArgs, onReject = [], resolvedArgs, state = 'pending',
+                onResolve = [],
+                rejectedArgs, onReject = [],
+                resolvedArgs, state = 'pending',
+                catchResolver,
                 reject = function(args) {
                     rejectedArgs = arguments;
                     for (var i = 0; i < onReject.length; i++) {
-                        if (typeof onReject[i] === 'function') {
-                            onReject[i].apply(pp, rejectedArgs);
-                        }
+                        onReject[i].apply(pp, rejectedArgs);
                     }
                     state = 'rejected';
                 },
                 resolve = function(args) {
                     resolvedArgs = arguments;
-                     for (var i = 0; i < onResolve.length; i++) {
-                   
-                    if (typeof onResolve[i] === 'function') {
+                    for (var i = 0; i < onResolve.length; i++) {
                         onResolve[i].apply(pp, resolvedArgs);
                     }
-                }
                     state = 'fulfilled';
                 };
             if (typeof fn === 'function') {
@@ -33,26 +39,58 @@
                     state = 'rejected';
                     rejectedArgs = [e];
                 }
+            } else {
+                throw (new Error('Promise argument ' + fn + ' is not a function'));
             }
-            pp.then = function(callback) {
-                if (typeof callback === 'function') {
-                    onResolve .push(callback);
-                    if (state === 'fulfilled') {
-                        callback.apply(pp, resolvedArgs);
+            pp.then = function(callback, fail) {
+
+                // Promise already rejected or resolved
+                if (state === 'fulfilled' && typeof callback === 'function') {
+                    return Promise.resolve(callback.apply(pp, resolvedArgs));
+                } else if (state === 'rejected' && typeof fail === 'function') {
+                    return Promise.reject(fail.apply(pp, rejectedArgs));
+
+                    // Promise resolved or rejected in future, return new Promise
+                    // which either resolves to return value of callback or fail, whichever executes
+                } else {
+                    if (typeof fail === 'function') {
+                        onReject.push(function() {
+                            thenResolver(fail.apply(pp, rejectedArgs));
+                        });
                     }
-                }
-                return pp;
-            };
-            pp.catch = function(fail) {
-                if (typeof fail === 'function') {
-                    onReject.push(fail);
-                    if (state === 'rejected') {
-                        fail.apply(pp, rejectedArgs);
+                    if (typeof callback === 'function') {
+                        onResolve.push(function() {
+                            thenResolver(callback.apply(pp, resolvedArgs));
+                        });
                     }
+                    return new PolyfillPromise(function(resolve) {
+                        thenResolver = resolve;
+                    });
                 }
-                return pp;
             };
-            return pp;
+            pp['catch'] = function(fail) {
+
+                // Promise already rejected or resolved
+                if (state === 'rejected' && typeof fail === 'function') {
+                    return PolyfillPromise.reject(fail.apply(pp, rejectedArgs));
+                } else if (state === 'fulfilled') {
+                    return PolyfillPromise.resolve(resolvedArgs);
+
+                    // Promise resolved or rejected in future, return new Promise
+                    // which either resolves to returned value of fail or the original fulfilled value.
+                } else {
+                    if (typeof fail === 'function') {
+                        onReject.push(function() {
+                            catchResolver(fail.apply(pp, rejectedArgs));
+                        });
+                    }
+                    onResolve.push(catchResolver);
+
+                    return new PolyfillPromise(function(resolve) {
+                        catchResolver = resolve;
+                    });
+                }
+            };
         };
         PolyfillPromise.all = function(array) {
             return new PolyfillPromise(function(resolve, reject) {
@@ -67,7 +105,7 @@
                             if (++resolved === size) {
                                 resolve(resolvedValues);
                             }
-                        }).catch(function(e) {
+                        })['catch'](function(e) {
                             if (!state) {
                                 state = 'rejected';
                                 reject(e);
@@ -91,12 +129,11 @@
                                 state = 'fulfilled';
                                 resolve(value);
                             }
-                        }).catch(function(e) {
+                        })['catch'](function(e) {
                             if (!state) {
                                 state = 'rejected';
                                 reject(e);
                             }
-
                         });
                     if (i < size - 1) {
                         iterate(++i);
@@ -107,14 +144,15 @@
         PolyfillPromise.resolve = function(objPromiseThenable) {
 
             // arg is a Promise
-            if (objPromiseThenable.constructor && objPromiseThenable.constructor.name === 'Promise') {
+            if (objPromiseThenable && objPromiseThenable.constructor && objPromiseThenable.constructor.name === 'Promise') {
                 return objPromiseThenable;
 
                 // arg is thenable    
-            } else if (typeof objPromiseThenable.then === 'function') {
+            } else if (objPromiseThenable && typeof objPromiseThenable.then === 'function') {
                 return new PolyfillPromise(function(resolve, reject) {
                     objPromiseThenable.then(resolve, reject);
                 });
+
                 // arg is object
             } else {
                 return new PolyfillPromise(function(resolve, reject) {
@@ -128,11 +166,14 @@
             });
         };
     } else {
+
+        // use native Promise if exists
+        newAble = null;
         PolyfillPromise = root.Promise;
     }
 
     // AMD module
-    if (typeof define === 'function' && define.amd) {
+    if (root.define && root.define.amd && typeof root.define === 'function' ) {
         define([], function() {
             return PolyfillPromise;
         });
